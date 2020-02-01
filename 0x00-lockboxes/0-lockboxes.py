d116d313c243f172f771de394065167bf2946dbb
nildiert <niljordan23@gmail.com>
2020-02-01 19:59:15 +0000

    Added README.md file

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This program checks if all boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """ This method determines if all boxes can be opened """
+
+    for box in boxes:
+        print(box)
