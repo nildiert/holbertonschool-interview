bd8aad729a0c8c5efabd411c3d91540591bed9d8
nildiert <niljordan23@gmail.com>
2020-02-03 20:19:55 +0000

    Fix one line

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,12 +5,16 @@
 def checkBox(box, matrix, keys_checked):
     """ This method checks all elements in a box """
 
-    if len(box) >= 0:
+    keys = [i for i in range(len(matrix))]
+    if len(box) > 0:
         for element in box:
             if element not in keys_checked:
                 if element < len(matrix):
                     keys_checked.append(element)
-                    checkBox(matrix[element], matrix, keys_checked)
+                    if (all(elem in keys_checked for elem in keys)):
+                        return True
+                    else:
+                        checkBox(matrix[element], matrix, keys_checked)
                 else:
                     return False
             else:
