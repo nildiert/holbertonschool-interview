079ba986f459b4b8b47f670bb38b9f097693339b
nildiert <niljordan23@gmail.com>
2020-02-03 17:57:20 +0000

    Added file 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,8 +2,27 @@
 """ This program checks if all boxes can be opened """
 
 
+def checkBox(box, matrix, keys_checked):
+    """ This method checks all elements in a box """
+
+    if len(box) > 0:
+        for element in box:
+            if element not in keys_checked:
+                print(element)
+                keys_checked.append(element)
+                checkBox(matrix[element], matrix, keys_checked)
+            else:
+                pass
+        return
+    else:
+        return
+
+
 def canUnlockAll(boxes):
     """ This method determines if all boxes can be opened """
 
-    for box in boxes:
-        print(box)
+    keys_checked = [0]
+    keys = [i for i in range(len(boxes))]
+
+    checkBox(boxes[0], boxes, keys_checked)
+    return (all(elem in keys_checked for elem in keys))
