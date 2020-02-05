e0bf801719090b41c0ae270be72b94e9bf000236
nildiert <niljordan23@gmail.com>
2020-02-05 00:28:47 +0000

    Added another version of boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,24 +2,26 @@
 """ This program checks if all boxes can be opened """
 
 
-def checkBox(box, matrix, keys_checked):
+def checkBox(box, matrix, keys_checked, keys, boxes_checked):
     """ This method checks all elements in a box """
 
-    keys = [i for i in range(len(matrix))]
+    if len(matrix) is 1 and len(matrix[0]) is 1:
+        return True
+
     if len(box) > 0:
         for element in box:
-            if element not in keys_checked:
-                if element < (len(matrix) - 1):
-                    keys_checked.append(element)
-                    if (all(elem in keys_checked for elem in keys)):
+            if element < len(matrix):
+                keys_checked[element] = 'Opened'
+                if element not in boxes_checked:
+                    boxes_checked.append(element)
+                    if len(boxes_checked) == len(matrix):
                         return True
-                    else:
-                        checkBox(matrix[element], matrix, keys_checked)
+                    checkBox(matrix[element], matrix, keys_checked, keys,
+                             boxes_checked)
                 else:
-                    return False
+                    pass
             else:
-                pass
-        return
+                return False
     else:
         return
 
@@ -27,13 +29,15 @@ def checkBox(box, matrix, keys_checked):
 def canUnlockAll(boxes):
     """ This method determines if all boxes can be opened """
 
-    keys_checked = [0]
-    keys = [i for i in range(len(boxes))]
+    keys = ['Opened' for i in range(len(boxes))]
+    keys_checked = ['Closed' for i in range(len(boxes))]
+    keys_checked[0] = 'Opened'
+    boxes_checked = [0]
 
-    if len(boxes) is 1:
-        return True
-    value = checkBox(boxes[0], boxes, keys_checked)
+    value = checkBox(boxes[0], boxes, keys_checked, keys, boxes_checked)
     if value is False:
         return False
-    else:
-        return (all(elem in keys_checked for elem in keys))
+    if value is True:
+        return True
+
+    return (all(elem in keys for elem in keys_checked))
