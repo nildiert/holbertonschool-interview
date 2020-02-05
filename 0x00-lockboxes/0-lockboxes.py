c8747450aa6f6da17c318aed7de7d3c33873712f
nildiert <niljordan23@gmail.com>
2020-02-05 02:03:59 +0000

    Changed all code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,48 +2,20 @@
 """ This program checks if all boxes can be opened """
 
 
-def checkBox(box, matrix, keys_checked, keys, boxes_checked):
-    """ This method checks all elements in a box """
-
-    if len(matrix) is 1 and len(matrix[0]) is 1:
-        return True
-    if type(box) is not list:
-        return False
-
-    if len(box) > 0:
-        for element in box:
-            if element < len(matrix):
-                keys_checked[element] = 'Opened'
-                if element not in boxes_checked:
-                    boxes_checked.append(element)
-                    if len(boxes_checked) == len(matrix):
-                        return True
-                    checkBox(matrix[element], matrix, keys_checked, keys,
-                             boxes_checked)
-                else:
-                    pass
-            else:
-                if len(boxes_checked) == len(matrix):
-                    return True
-                return False
-    else:
-        return
+def checkBox(index, key, boxes):
+    """ This method determines if all boxes can be opened  """
+    return (key in boxes[index] and key != index)
 
 
 def canUnlockAll(boxes):
     """ This method determines if all boxes can be opened """
 
-    keys = ['Opened' for i in range(len(boxes))]
-    keys_checked = ['Closed' for i in range(len(boxes))]
-    keys_checked[0] = 'Opened'
-    boxes_checked = [0]
-
-    value = checkBox(boxes[0], boxes, keys_checked, keys, boxes_checked)
-    if type(boxes) is not list or len(boxes) == 0:
-        return False
-    if value is False:
-        return False
-    if value is True:
-        return True
-
-    return (all(elem in keys for elem in keys_checked))
+    for key in range(1, len(boxes) - 1):
+        flag = False
+        for index in range(len(boxes)):
+            flag = checkBox(index, key, boxes)
+            if flag:
+                break
+        if flag is False:
+            return flag
+    return True
