b7446cb5ab34021254e12fcad3a4896809e0eb80
nildiert <niljordan23@gmail.com>
2020-02-24 18:52:11 +0000

    Added file 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Min value """
+
+
+def prime(number):
+    """ Check if number is prime """
+    if number <= 2:
+        return True
+    else:
+        i = 1
+        value = 0
+        while i <= number:
+            if number % i == 0:
+                value += 1
+            if value > 2:
+                return False
+            i += 1
+        return True
+
+
+def factor(num):
+    """ Return factors of number """
+    prime_list = []
+    value = num
+    i = 1
+    while value != 1:
+        i += 1
+        if prime(i):
+            if value % i == 0:
+                while (value % i == 0 and value != 1):
+                    value /= i
+                    prime_list.append(i)
+
+    return prime_list
+
+
+def minOperations(n):
+    if not prime(n):
+        values = factor(n)
+        return sum(values)
+    else:
+        return n
