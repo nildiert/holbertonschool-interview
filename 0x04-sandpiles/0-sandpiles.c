e9763303ca68bf3590f7e3ed75aa555c1e584b7b
nildiert <niljordan23@gmail.com>
2020-03-03 22:47:00 +0000

    Added file 0

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sandpiles.h"
+
+/**
+ * print_grid - computes the sum of two sandpiles
+ * @grid: sandpiles stable
+ * Return: void function
+ */
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * sum_grids - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
+ * Return: void function
+ */
+void sum_grids(int grid1[3][3], int grid2[3][3])
+{
+	int i;
+	int j;
+
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+		}
+	}
+}
+
+/**
+ * change_grid - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * Return: void function
+ */
+void change_grid(int grid1[3][3])
+{
+	int i;
+	int j;
+	int new_grid[3][3];
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			new_grid[i][j] = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				if ((i - 1 >= 0) && (i - 1 < 3))
+					new_grid[i - 1][j] += 1;
+				if ((j - 1 >= 0) && (j - 1 < 3))
+					new_grid[i][j - 1] += 1;
+				if ((i + 1 >= 0) && (i + 1 < 3))
+					new_grid[i + 1][j] += 1;
+				if ((j + 1 >= 0) && (j + 1 < 3))
+					new_grid[i][j + 1] += 1;
+			}
+		}
+	}
+
+	sum_grids(grid1, new_grid);
+}
+
+
+/**
+ * check_stability - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * Return: void function
+ */
+int check_stability(int grid1[3][3])
+{
+
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid1[i][j] > 3)
+				return (0);
+	return (1);
+}
+
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
+ * Return: void function
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+	sum_grids(grid1, grid2);
+
+	while (!check_stability(grid1))
+	{
+		print_grid(grid1);
+		change_grid(grid1);
+	}
+
+}
