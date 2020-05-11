f4d774ce7b7cdee2358f2f804b6015e7e0d240e7
nildiert <niljordan23@gmail.com>
2020-05-11 00:24:19 +0000

    [FEATURE] Fixed shebang

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 N Queens
 """
@@ -121,7 +121,6 @@ def validate(args):
 
 if __name__ == "__main__":
     """ Main method to execute the application
-
     """
 
     number = validate(sys.argv)
