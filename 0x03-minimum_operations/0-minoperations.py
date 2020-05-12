b1c329fbb8d1396e2253af058ec71a7587989826
Nildiert <niljordan23@gmail.com>
2020-05-11 22:18:49 -0500

    [TEST] Test code

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -20,27 +20,19 @@ import math
 
 
 def factors(n):
-
-    factors = []
-    # Print the number of two's that divide n
+    """calculates the factors of n number"""
+    mylist = []
     while n % 2 == 0:
-        factors.append(2)
+        mylist.append(2)
         n = n / 2
-
-    # n must be odd at this point
-    # so a skip of 2 ( i = i + 2) can be used
-    for i in range(3, int(math.sqrt(n))+1, 2):
-
-        # while i divides n , print i ad divide n
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         while n % i == 0:
-            factors.append(i)
+            mylist.append(i)
             n = n / i
-
-    # Condition if n is a prime
-    # number greater than 2
     if n > 2:
-        factors.append(n)
-    return factors
+        mylist.append(n)
+    return mylist
+
 
 
 def minOperations(n):
