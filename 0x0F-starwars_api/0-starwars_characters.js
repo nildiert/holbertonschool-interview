e9f9b4a61d4dd1bce594f9bf4a860de40e7c1d65
nildiert <niljordan23@gmail.com>
2020-06-02 17:59:54 +0000

    [FEATURE] Added condition for error in request

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,18 +6,22 @@ try {
   const movieId = process.argv[2];
   const url = `https://swapi.dev/api/films/${movieId}/`;
   request(url, async function (error, response, body) {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (err, res, html) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(html).name);
-          }
+    if (error) {
+      console.log(error);
+    } else {
+      const characters = JSON.parse(body).characters;
+      for (const character of characters) {
+        const res = await new Promise((resolve, reject) => {
+          request(character, (err, res, html) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(html).name);
+            }
+          });
         });
-      });
-      console.log(res);
+        console.log(res);
+      }
     }
   });
 } catch (catchErr) {
