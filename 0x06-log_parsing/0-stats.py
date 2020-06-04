1cdc6680973ff3b7fdc6ecba3102207aee2c4158
Nildiert <niljordan23@gmail.com>
2020-06-04 00:01:36 -0500

    gsd

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""
+This script  reads stdin line by line and computes metrics
+"""
 
 import sys
 
 
-i = 0
-sum_file_size = 0
-status_code = {200: 0,
-               301: 0,
-               400: 0,
-               401: 0,
-               403: 0,
-               404: 0,
-               405: 0,
-               500: 0}
+total_size = 0
+counter = 0
+status_code = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0,
+
+}
 
 try:
     for line in sys.stdin:
-        args = line.split(' ')
-        if len(args) > 2:
-            status_line = args[-2]
-            file_size = args[-1]
-            if status_line in status_code:
-                status_code[status_line] += 1
-            sum_file_size += int(file_size)
-            i += 1
-            if i == 10:
-                print('File size: {:d}'.format(sum_file_size))
-                sorted_keys = sorted(status_code.keys())
-                for key in sorted_keys:
-                    value = status_code[key]
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                i = 0
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            size = line_list[-1]
+            code = line_list[-2]
+            if code in status_code:
+                status_code[code] += 1
+
+            total_size += int(size)
+            counter += 1
+            if counter == 10:
+                print("File size: {:d}".format(total_size))
+                s_code = sorted(status_code.keys())
+                for key in s_code:
+                    if status_code[key] is not 0:
+                        print("{}: {}".format(key, status_code[key]))
+                counter = 0
 except Exception:
     pass
 finally:
-    print('File size: {:d}'.format(sum_file_size))
-    sorted_keys = sorted(status_code.keys())
-    for key in sorted_keys:
-        value = status_code[key]
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print("File size: {}".format(total_size))
+    s_code = sorted(status_code.keys())
+    for key in s_code:
+        if status_code[key] is not 0:
+            print("{}: {}".format(key, status_code[key]))
