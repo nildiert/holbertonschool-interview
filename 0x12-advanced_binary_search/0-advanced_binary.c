6674219cb07a029deaafbd6f29b97a92ddbfe276
nildiert <niljordan23@gmail.com>
2020-06-24 23:06:50 +0000

    [UPDATE] Changed iteration by recursive function

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -20,6 +20,39 @@ void print_array(int *array, int first, int last)
 	printf("\n");
 }
 
+/**
+ * rec_bin - Recursive advanced binary search
+ * @array: Array
+ * @first: First element of array
+ * @last: Last element of array
+ * @value: Value to find
+ * Return: Always EXIT_SUCCESS
+ */
+
+int rec_bin(int *array, size_t first, size_t last, int value)
+{
+	size_t half;
+
+	if (first <= last)
+	{
+		half = first + (last - first) / 2;
+		print_array(array, (int)first, (int)last);
+		if (array[half] == value)
+		{
+			if (array[half - 1] == value)
+				return (rec_bin(array, first, half, value));
+			else
+				return ((int)(half));
+		}
+		if (array[half] < value)
+			return (rec_bin(array, half + 1, last, value));
+		else
+			return (rec_bin(array, first, half - 1, value));
+	}
+	else
+		return (-1);
+}
+
 
 /**
  * advanced_binary - Entry point
@@ -32,40 +65,13 @@ void print_array(int *array, int first, int last)
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t first;
-	size_t half;
 	size_t last;
-	size_t first_value;
 
 	if (!array)
 		return (-1);
 
-	first_value = 0;
 	first = 0;
 	last = size - 1;
-	while (first <= last)
-	{
-		print_array(array, (int)first, (int)last);
-		half = first + (last - first) / 2;
-		if (array[half] == value)
-		{
-			if (array[half - 1] == value)
-				first_value = 1;
-			else
-				return ((int)(half));
-		}
-		if (array[half] < value)
-			first = half + 1;
-		else
-		{
-			if (first_value)
-			{
-				last = half;
-				first_value = 0;
-			}
-			else
-				last = half - 1;
-		}
-	}
+	return (rec_bin(array, first, last, value));
 
-	return (-1);
 }
