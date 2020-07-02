3a4ab268e6528ce73bf97d876e3ce243b2071e26
nildiert <niljordan23@gmail.com>
2020-07-01 23:55:37 -0500

    [FEATURE] Test

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,136 +1,102 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "slide_line.h"
+#include <stdlib.h>
 
 /**
- * slide_zeros_left - Remove zeros
+ * left - Move the direction to the left
  *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
+ * @line: points to an array of integers containing size elements
+ * @size: Number of elements in @array
  */
 
-void slide_zeros_left(int *line, size_t size)
+void left(int *line, size_t size)
 {
-	int a, z, aux, index;
-	int count_zeros;
-	int lenght;
-	lenght = size;
-	for (a = 0; a < lenght; a++)
+	size_t i, j, k;
+
+	for (i = 0, j = 0; i < size;)
 	{
-		count_zeros = 0;
-		z = a;
-		while (line[z] == 0 && z < lenght)
+		if (line[i] == 0)
+			for (; line[i] == 0; i++);
+		if (i == (size - 1))
+			break;
+		j = i + 1;
+		if (line[j] == 0)
+			for (; line[j] == 0; j++);
+		if (line[i] == line[j])
 		{
-			count_zeros += 1;
-			z++;
+			line[i] = line[i] + line[j];
+			line[j] = 0;
+			i = j + 1;
 		}
-		index = a + count_zeros;
-		if (index < lenght)
+		else
+			i++;
+		j = 0;
+	}
+	for (i = 0, k = 0; i < size; i++)
+		if (line[i] != 0)
+			line[k++] = line[i];
+	while (k < size)
+		line[k++] = 0;
+}
+
+
+/**
+ * right - Move the direction to the left
+ *
+ * @line: points to an array of integers containing size elements
+ * @size: Number of elements in @array
+ */
+
+void right(int *line, size_t size)
+{
+	size_t i, j;
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = i - 1; j > 0; j--)
 		{
-			aux = line[index];
-			line[index] = line[a];
-			line[a] = aux;
+			if (line[i - 1] != 0)
+			{
+				if (line[i - 1] == line[j - 1])
+				{
+					line[i - 1] = line[i - 1] + line[j - 1];
+					line[j - 1] = 0;
+					break;
+				}
+				if (line[i - 1] != line[j - 1] &&
+				    line[j - 1] != 0)
+					break;
+			}
+			if (line[i - 1] == 0)
+			{
+				if (line[j - 1] != 0)
+				{
+					line[i - 1] = line[j - 1];
+					line[j - 1] = 0;
+					i++;
+					break;
+				}
+			}
 		}
 	}
 }
 
-/**
- * slide_zeros_right - Remove zeros
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- */
-
-void slide_zeros_right(int *line, size_t size)
-{
-	int a, z, aux, index;
-	int count_zeros;
-	for (a = size - 1; a >= 0; a--)
-	{
-		count_zeros = 0;
-		z = a;
-		while (line[z] == 0 && z >= 0)
-		{
-			count_zeros += 1;
-			z--;
-		}
-		index = a - count_zeros;
-		aux = line[index];
-		line[index] = line[a];
-		line[a] = aux;
-	}
-}
 
 /**
- * add_numbers_left - Add numbers
+ * slide_line - Slide and merge lines
  *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- */
-
-void add_numbers_left(int *line, size_t size)
-{
-	int next, i, length;
-	length = size;
-	for (i = 0; i < length - 1; i++)
-	{
-		next = i + 1;
-		if (line[i] == line[next])
-		{
-			line[i] = line[i] + line[next];
-			line[next] = 0;
-		}
-	}
-}
-
-/**
- * add_numbers_right - Add numbers
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- */
-
-void add_numbers_right(int *line, size_t size)
-{
-	int behind, i, length;
-	length = size;
-	for (i = length - 1; i > 0; i--)
-	{
-		behind = i - 1;
-		if (line[i] == line[behind])
-		{
-			line[i] = line[i] + line[behind];
-			line[behind] = 0;
-		}
-	}
-}
-
-/**
- * slide_line - 2048 game
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- * @direction: Direction of the slide
+ * @line: points to an array of integers containing size elements
+ * @size: Number of elements in @array
+ * @direction: Number of elements in @array
+ * Return: Number of elements in 1
  */
 
 int slide_line(int *line, size_t size, int direction)
 {
-	if (direction == 1)
-	{
-		slide_zeros_right(line, size);
-		add_numbers_right(line, size);
-		slide_zeros_right(line, size);
-	}
-	else if (direction == 1)
-	{
-		slide_zeros_left(line, size);
-		add_numbers_left(line, size);
-		slide_zeros_left(line, size);
-	}
-	else
-	{
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
 		return (0);
-	}
+	if (direction == 0)
+		left(line, size);
+	if (direction == 1)
+		right(line, size);
 	return (1);
 }
