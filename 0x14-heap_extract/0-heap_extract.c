6626eb163beea01bd02dfc568b83c42f495dad88
nildiert <niljordan23@gmail.com>
2020-09-24 00:26:20 +0000

    [UPDATE] Updated Heap Extract

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,44 +1,101 @@
 #include "binary_trees.h"
 
 /**
+ * _height - CHeck the height of a binary tree
+ *
+ * @tree: Pointer to the node to measures the height
+ *
+ * Return: The height of the tree starting at @node
+ */
+static size_t _height(const binary_tree_t *tree)
+{
+	size_t height_left;
+	size_t height_right;
+
+	height_left = tree->left ? 1 + _height(tree->left) : 0;
+	height_right = tree->right ? 1 + _height(tree->right) : 0;
+	return (height_left > height_right ? height_left : height_right);
+}
+
+/**
+ * _preorder - goes through a binary tree using pre-order traversal
+ * @root: pointer root of the tree
+ * @node: pointer node in the tree
+ * @h: height of tree
+ * @l: layer on the tree
+ **/
+void _preorder(heap_t *root, heap_t **node, size_t h, size_t l)
+{
+	if (!root)
+		return;
+	if (h == l)
+		*node = root;
+	l++;
+	if (root->left)
+		_preorder(root->left, node, h, l);
+	if (root->right)
+		_preorder(root->right, node, h, l);
+}
+
+/**
+ * _sorting - binary tree Heapsort
+ * @temp: pointer to the heap root
+ * Return: pointer to last node
+ **/
+
+heap_t *_sorting(heap_t *temp)
+{
+	int aux;
+
+	while (temp->left || temp->right)
+	{
+		if (!temp->right || temp->left->n > temp->right->n)
+		{
+			aux = temp->n;
+			temp->n = temp->left->n;
+			temp->left->n = aux;
+			temp = temp->left;
+		}
+		else if (!temp->left || temp->left->n < temp->right->n)
+		{
+			aux = temp->n;
+			temp->n = temp->right->n;
+			temp->right->n = aux;
+			temp = temp->right;
+		}
+
+	}
+	return (temp);
+}
+
+/**
  * heap_extract - extracts the root node of a Max Binary Heap
- * @root: pointer to root
+ * @root: pointer to the heap root
  * Return: value of extracted node
  **/
+
 int heap_extract(heap_t **root)
 {
-	int value, value_temp;
-	heap_t *temp;
+	int value;
+	heap_t *aux, *node;
 
 	if (!root || !*root)
 		return (0);
-	temp = *root;
-	value = temp->n;
-	if (!temp->left && !temp->right && !temp->parent)
+	aux = *root;
+	value = aux->n;
+	if (!aux->left && !aux->right)
 	{
-		free(temp);
+		*root = NULL;
+		free(aux);
 		return (value);
 	}
-	while (temp->left || temp->right)
-	{
-		if (temp->left && temp->right)
-		{
-			if (!temp->right || temp->left->n > temp->right->n)
-			{
-				value_temp = temp->n;
-				temp->n = temp->left->n;
-				temp->left->n = value_temp;
-				temp = temp->left;
-			}
-			else if (temp->left->n < temp->right->n)
-			{
-				value_temp = temp->n;
-				temp->n = temp->right->n;
-				temp->right->n = value_temp;
-				temp = temp->right;
-
-			}
-		}
-	}
+	_preorder(aux, &node, _height(aux), 0);
+	aux = _sorting(aux);
+	aux->n = node->n;
+	if (node->parent->right)
+		node->parent->right = NULL;
+	else
+		node->parent->left = NULL;
+	free(node);
 	return (value);
 }
